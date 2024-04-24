38f2a4d93c6df21bdf9f8f6d5f16555fe436e624
Eve-Lyne Gagnon <eve-lyne_gagnon@hotmail.ca>
2024-04-24 15:22:18 -0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,91 @@
+const icon = document.querySelector('#icon');
+const button = document.querySelector('#button');
+const textIcon = document.querySelector('.text__icon');
+
+
+/**
+ * @description Fonction pour changer la couleur de fond pour violet si plus petit que 768px, et vert si plus grand ou égal.
+ * @param {MediaQueryList} mediaQueryList 
+ */
+
+function switchBackgroundColor(mediaQueryList) {
+    mediaQueryList.matches ? document.body.style.backgroundColor = 'violet' : document.body.style.backgroundColor = 'green';
+}
+
+const mediaQueryList768px = window.matchMedia('(min-width: 768px)');
+
+switchBackgroundColor(mediaQueryList768px);
+
+mediaQueryList768px.addEventListener('change', () => {switchBackgroundColor(mediaQueryList768px)});
+
+
+
+/**
+ * @description Fonction pour faire apparaître le bouton si plus grand que 1024px.
+ * @param {MediaQueryList} mediaQueryList 
+ */
+
+function showButton(mediaQueryList) {
+    if (mediaQueryList.matches) {
+        button.classList.remove('hidden');
+    }
+    button.classList.toggle('hidden');
+}
+
+const mediaQueryList1024px = window.matchMedia('(max-width: 1024px)');
+
+showButton(mediaQueryList1024px);
+
+mediaQueryList1024px.addEventListener('change', () => {showButton(mediaQueryList1024px)});
+
+
+/**
+ * @description Fonction pour faire apparaître l'icône si plus petit que 640px. Si plus grand ou égal, on affiche le string "connection".
+ * @param {MediaQueryList} mediaQueryList 
+ */
+
+function showIcon(mediaQueryList) {
+    if (mediaQueryList.matches) {
+        icon.classList.remove('hidden');
+    } else {
+        textIcon.classList.remove('hidden');
+        textIcon.textContent = 'connection';
+    }
+    icon.classList.toggle('hidden');
+    textIcon.classList.toggle('hidden');
+}
+
+const mediaQueryList640px = window.matchMedia('(min-width: 640px)');
+
+showIcon(mediaQueryList640px);
+
+mediaQueryList640px.addEventListener('change', () => {showIcon(mediaQueryList640px)});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Notes*/
+/*function switchBackgroundColor(mediaQueryList) {
+    if(mediaQueryList.matches) {
+        document.body.style.flexDirection = 'row';
+    } else {
+        document.body.style.flexDirection = 'column';
+    }
+}
+
+const mediaQueryList = window.matchMedia('(min-width: 700px)');
+
+switchBackgroundColor(mediaQueryList);
+
+mediaQueryList.addEventListener('change', () => {switchBackgroundColor(mediaQueryList)}); */
+
